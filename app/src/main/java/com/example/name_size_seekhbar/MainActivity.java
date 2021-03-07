16f828d6db2dec9432dd594eb3b92d22e807b118
CH-79 <hammadkhalid833@gmail.com>
2021-03-07 12:04:25 +0500

    7-3-21

---
+++ app/src/main/java/com/example/name_size_seekhbar/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.name_size_seekhbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView tview;
+    private int progressChangedValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar=findViewById(R.id.seekBar);
+        tview=findViewById(R.id.tview);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue=progress;
+                tview.setTextSize(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
